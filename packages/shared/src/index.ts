bb8fdbf214e9d5d80d702a9f729622ad3a04b447
Le Thanh Tung <thanhtung22012006@gmail.com>
2026-03-13 23:15:28 +0700

    update weather mobile

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,30 +1,38 @@
-
 // Types
-export * from './types';
+export * from "./types";
 
 // Constants
-export * from './constants';
+export * from "./constants";
 
 // Utilities
-export * from './utils';
+export * from "./utils";
 
 // Schemas
-export * from './schemas';
+export * from "./schemas";
 
 // Explicit exports to ensure TS sees them (fixes missing-export errors)
-export { APP_DESCRIPTION } from './constants/app.constants';
-export { isValidImageFile } from './utils/validation.utils';
-export type { IScanResult, IScanHistoryDetail, IYoloPrediction } from './types/scan-history.types';
-export { forgotPasswordSchema, type ForgotPasswordFormData } from './schemas/auth.schema';
+export { APP_DESCRIPTION } from "./constants/app.constants";
+export { isValidImageFile } from "./utils/validation.utils";
+export type {
+  IScanResult,
+  IScanHistoryDetail,
+  IYoloPrediction,
+} from "./types/scan-history.types";
+export {
+  forgotPasswordSchema,
+  type ForgotPasswordFormData,
+} from "./schemas/auth.schema";
 
 // Legacy export (để không break code cũ)
 
-export * from './api/token-manager';
-export * from './api/scan.api';
-export * from './api/axios-client';
-export * from './api/plant.api';
-export * from './api/auth.api';
-export * from './api/user.api';
+export * from "./api/token-manager";
+export * from "./api/scan.api";
+export * from "./api/axios-client";
+export * from "./api/plant.api";
+export * from "./api/auth.api";
+export * from "./api/user.api";
+export * from "./api/weather.api";
+export * from "./types/weather.types";
 export const helloWorld = () => {
   return "Hello from Shared Logic!";
 };
